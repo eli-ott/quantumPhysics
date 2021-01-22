4c0dbc4c80fe208bd555770d0e9bdc79cb1f9192
eli-ott <eli-ott@users.noreply.github.com>
2021-01-22 10:52:39 +0100

    commit to 'main'

--- script/darkMode.js
+++ script/darkMode.js
@@ -1,7 +1,11 @@
 const darkMode = () => {
     //we change the variable of the background color and the main color
-    document.documentElement.style.setProperty('--backgroundColor', '#fcffeb');
-    document.documentElement.style.setProperty("--color", "#242424");
+    document.documentElement.style.setProperty('--backgroundColor', '#242424');
+    document.documentElement.style.setProperty("--color", "#fcffeb");
+
+    //we change the color of the menu icon
+    $("#topLine").css("stroke", "#fcffeb");
+    $("#bottomLine").css("stroke", "#fcffeb");
 
     //we change the moon icon for the sun
     $("#moon").fadeOut(250);
@@ -12,10 +16,15 @@ const darkMode = () => {
     //we set a localStorage to remember the prefered theme by the user
     localStorage.setItem("theme", "dark");
 }
+
 const lightMode = () => {
     //we change the css variable that refer to the background color and the color
     document.documentElement.style.setProperty('--backgroundColor', '#fcffeb');
     document.documentElement.style.setProperty('--color', '#242424');
+
+    //we change the color of the menu icon
+    $("#topLine").css("stroke", "#242424");
+    $("#bottomLine").css("stroke", "#242424");
 
     //we chhange the sun icon for a moon icon 
     $("#sun").fadeOut(250);
