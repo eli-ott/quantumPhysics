f086a9e803b4edf6fb00744d2112f863031c5807
eli-ott <eli-ott@users.noreply.github.com>
2021-01-24 14:16:56 +0100

    commit to 'main'

--- script/atomAnim.js
+++ script/atomAnim.js
@@ -1,15 +1,31 @@
+let scrolled = true;
+$(window).on("scroll", () => {
+    let yOffset = window.pageYOffset;
+    if(yOffset == 0){
+        scrolled = true;
+    } else{
+        scrolled = false;
+    }
+    electronsRotation();
+}); 
+
 let largeRotation = 0;
-setInterval(() => {
-    largeRotation == 360 ? largeRotation = 0: largeRotation += 0.1; 
-    $(".largeOrbitAtom").attr("transform", `rotate(${largeRotation}, 250, 250)`);
-}, 10);
-let mediumRotation = 0;
-setInterval(() => {
-    mediumRotation == 360 ? mediumRotation = 0: mediumRotation += 0.5; 
-    $(".mediumOrbitAtom").attr("transform", `rotate(${mediumRotation}, 250, 250)`);
-}, 10);
-let smallOrbitRotation = 0;
-setInterval(() => {
-    smallOrbitRotation == 360 ? smallOrbitRotation = 0: smallOrbitRotation += 1.5; 
-    $(".smallOrbitAtom").attr("transform", `rotate(${smallOrbitRotation}, 250, 250)`);
-}, 10);
+
+const electronsRotation = () => {
+    if(scrolled){
+    window.largeInterval = setInterval(() => {
+        largeRotation == 360 ? largeRotation = 0: largeRotation += 0.1; 
+        $(".largeOrbitAtom").attr("transform", `rotate(${largeRotation}, 250, 250)`);
+    }, 10);
+    let mediumRotation = 0;
+    window.mediumInterval = setInterval(() => {
+        mediumRotation == 360 ? mediumRotation = 0: mediumRotation += 0.5; 
+        $(".mediumOrbitAtom").attr("transform", `rotate(${mediumRotation}, 250, 250)`);
+    }, 10);
+    } else{
+        clearInterval(mediumInterval);
+        clearInterval(largeInterval);
+    }
+}
+
+electronsRotation();
