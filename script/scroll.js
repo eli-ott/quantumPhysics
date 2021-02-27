100bfa810cad0fd8b5ac9a0a77d1078ef55dac66
eli-ott <eli-ott@users.noreply.github.com>
2021-02-27 10:09:16 +0100

    added a progress bar

---
+++ script/scroll.js
@@ -0,0 +1,10 @@
+//we define the heaight page when it's ready
+$(window).ready(() => {
+    window.contentHeight =  parseInt($("footer").css("top")) - $("footer").height() - 100;
+});
+
+//whe change the width of the div#progress when the user scroll
+$(window).on("scroll", () => {
+    let pageYOffsset = window.pageYOffset;
+    $("#progress").css("width", `${(pageYOffsset * 100) / contentHeight}%`);
+});
